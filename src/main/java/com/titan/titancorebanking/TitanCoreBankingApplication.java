203ff9ddde0f093d94e30e83d50da14767f196b1
Bunchhay1 <kaingbunchhay1@gmail.com>
2025-12-30 01:28:07 +0700

    feat: Connect Spring Boot to Remote Proxmox Postgres DB

---
+++ src/main/java/com/titan/titancorebanking/TitanCoreBankingApplication.java
@@ -0,0 +1,13 @@
+package com.titan.titancorebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanCoreBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitanCoreBankingApplication.class, args);
+    }
+
+}
